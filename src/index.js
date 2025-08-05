caeb8992ec42a3b4519b7e358078f5474babffea
Harishma <harishma2024@gmail.com>
2025-08-05 14:52:52 +0530

    handled api response and errors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ dotenv.config({
     path: "./.env"
 })
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("ERROR: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("DB Connection failed !!!", error)
+})
 
 
 // import express from "express";
